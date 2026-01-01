95cba8b6f08bb17c90206e826a2efcf1a09c59b7
עומר רודנאי <33462123+potzko@users.noreply.github.com>
2026-01-01 10:31:30 +0200

    added tests, removed code smell

--- digitstonum.py
+++ digitstonum.py
@@ -1,3 +1,5 @@
+import functools
+
 under_twenty = [
     'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
     'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen',
@@ -7,6 +9,13 @@ under_twenty = [
 thousand_pows = [
     '', 'thousand', 'million', 'billion', 'trillion', 'quadrillion', 'quintillion'
 ]
+
+tens = [
+    '', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'
+]
+
+def line_on_page(digits: int) -> str:
+    return digit_to_num(digits) + '\n'
 
 def digit_to_num(digits: int) -> str:
     if 0 <= digits <= 20:
@@ -24,17 +33,15 @@ def digit_to_num(digits: int) -> str:
         if thousand_pows[idx]:
             part_word += f" {thousand_pows[idx]}"
         word_parts.append(part_word)
-    return ' '.join(reversed(word_parts)) 
+    return ' '.join(reversed(word_parts))
 
+@functools.cache
 def under_hundred(digits: int) -> str:
     assert digits < 100
-    tens = [
-        '', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'
-    ]
     if digits < 21:
         return under_twenty[digits]
     
-    assert 20 <digits < 100
+    assert 20 < digits < 100
     ten_part = tens[digits // 10]
     unit_part = digits % 10
     if unit_part == 0:
@@ -42,6 +49,7 @@ def under_hundred(digits: int) -> str:
     else:
         return f"{ten_part} {under_twenty[unit_part]}"
 
+@functools.cache
 def under_thousand(digits: int) -> str:
     assert digits < 1000
     if digits < 100:
@@ -52,7 +60,7 @@ def under_thousand(digits: int) -> str:
     if rest_part == 0:
         return f"{under_twenty[hundred_part]} hundred"
     else:
-        return f"{under_twenty[hundred_part]} hundred and {digit_to_num(rest_part)}"
+        return f"{under_twenty[hundred_part]} hundred {digit_to_num(rest_part)}"
 
 def break_into_thousands(digits: int) -> list[int]:
     parts = []
@@ -62,15 +70,42 @@ def break_into_thousands(digits: int) -> list[int]:
     return parts
 
 def main():
-    test_numbers = [
-        0, 5, 13, 20, 25, 42, 99, 100, 105, 215, 999,
-        1000, 1500, 12345, 1000000, 2000001, 3050607,
-        1234567890, 1000000000000
-    ]
-    for number in test_numbers:
-        print(f"{number}: {digit_to_num(number)}")
-    print(f"total chars under a million: {sum(len(digit_to_num(n)) + 1 for n in range(1, 1000000))}") # the +1 is for lines 
-
+    #tests
+    assert line_on_page(0) == 'zero\n'
+    assert line_on_page(5) == 'five\n'
+    assert line_on_page(13) == 'thirteen\n'
+    assert line_on_page(20) == 'twenty\n'
+    assert line_on_page(25) == 'twenty five\n'
+    assert line_on_page(42) == 'forty two\n'
+    assert line_on_page(99) == 'ninety nine\n'
+    assert line_on_page(100) == 'one hundred\n'
+    assert line_on_page(105) == 'one hundred five\n'
+    assert line_on_page(215) == 'two hundred fifteen\n'
+    assert line_on_page(999) == 'nine hundred ninety nine\n'
+    assert line_on_page(1000) == 'one thousand\n'
+    assert line_on_page(1500) == 'one thousand five hundred\n'
+    assert line_on_page(12345) == 'twelve thousand three hundred forty five\n'
+    assert line_on_page(1000000) == 'one million\n'
+    assert line_on_page(1123123) == 'one million one hundred twenty three thousand one hundred twenty three\n'
+    assert line_on_page(2000001) == 'two million one\n'
+    assert line_on_page(3050607) == 'three million fifty thousand six hundred seven\n'
+    assert line_on_page(1234567890) == 'one billion two hundred thirty four million five hundred sixty seven thousand eight hundred ninety\n'
+    assert line_on_page(1234567890123) == 'one trillion two hundred thirty four billion five hundred sixty seven million eight hundred ninety thousand one hundred twenty three\n'
+    def get_text(n: int = 1_000_001) -> str:
+        return (line_on_page(i) for i in range(n))
+    
+    print(f"total chars under a million: {sum(len(word) for word in get_text())}")
+    print(f"total letters under a million: {sum(len([i for i in word if not i in [' ', '\n']]) for word in get_text())}")
+    print(f"total spaces and new lines under a million: {sum(len([i for i in word if i in [' ', '\n']]) for word in get_text())}")
+    seen = set()
+    for i in range(1000001):
+        word = line_on_page(i)
+        chars = set(word)
+        if chars - seen != set():
+            print(f"the first number where the letters {chars - seen} appear is {i}")
+        seen = seen | chars
+    print(f"unique chars used: {seen}")
+    print(f"number of unique chars used: {len(seen)}")
 
 if __name__ == "__main__":
     main()
